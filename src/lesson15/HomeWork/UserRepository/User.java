9913c8ccf978a090179164a527bf2674b90c5da7
dymychUA <dymych@outlook.com>
2018-08-22 13:51:20 +0300

    lesson 15 HW - 6

--- src/lesson15/HomeWork/UserRepository/User.java
+++ src/lesson15/HomeWork/UserRepository/User.java
@@ -40,9 +40,13 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return id == user.id &&
-                Objects.equals(name, user.name) &&
-                Objects.equals(sessionId, user.sessionId);
+        if (!name.equals(user.name)) return false;
+        if (!sessionId.equals(user.sessionId)) return false;
+        return id == user.id;
+
+//        return id == user.id &&
+//                Objects.equals(name, user.name) &&
+//                Objects.equals(sessionId, user.sessionId);
     }
 
     @Override
