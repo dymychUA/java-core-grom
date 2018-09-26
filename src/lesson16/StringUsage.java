8bad4c78ae737bfe40ef7ac096a95ccf80d451e1
dymychUA <dymych@outlook.com>
2018-09-26 13:41:26 +0300

    lesson 15 HW - 7

--- src/lesson16/StringUsage.java
+++ src/lesson16/StringUsage.java
@@ -2,13 +2,14 @@ package lesson16;
 
 public class StringUsage {
     public static void main(String[] args) {
+
         String test = "someString";
         String result = test + "_end";
 
         System.out.println(result);
 
         char charVariable = 't';
-        char charVariable1 = '0';
+        char charVariable1 = 'o';
 
         char[] chars = {charVariable, charVariable1};
 
