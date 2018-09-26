5e8566b8403813d0d7002cf07aae48a46d622a5c
dymychUA <dymych@outlook.com>
2018-09-26 19:36:00 +0300

    lesson 15 HW - 7

---
+++ src/lesson17/Solution.java
@@ -0,0 +1,165 @@
+package lesson17;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        String input = "Test input string input demo in input in";
+
+        //1
+        System.out.println(countWords(input));
+
+        //2
+        System.out.println(maxWord(input));
+        System.out.println(minWord(input));
+
+        //3
+        System.out.println(mostCountedWord(input));
+
+        //4
+        System.out.println(validate("https://ukr.net"));
+        System.out.println(validate("www.spb.ru"));
+    }
+
+
+    //1
+    public static int countWords(String input) {
+        return input.split(" ").length;
+    }
+
+
+    //2
+    public static String maxWord(String input) {
+
+        if (input.equals(""))
+            return "";
+
+        String[] words = input.split(" ");
+        String res = words[0];
+
+        for (String word : words) {
+            if (res.length() < word.length())
+                res = word;
+        }
+
+        return res;
+    }
+
+    public static String minWord(String input) {
+
+        if (input.equals(""))
+            return "";
+
+        String[] words = input.split(" ");
+        String res = words[0];
+
+        for (String word : words) {
+            if (res.length() > word.length())
+                res = word;
+        }
+
+        return res;
+    }
+
+
+    //3
+    public static String mostCountedWord(String input) {
+
+        if (input == null || input.equals(""))
+            return null;
+
+        String[] words = deleteDuplicates(input).split(" ");
+        int[] counters = countDuplicates(input, words);
+
+        int maxIndex = 0;
+        int maxValue = counters[0];
+        for (int i = 0; i < counters.length; i++) {
+            if (counters[i] > maxValue) {
+                maxIndex = i;
+                maxValue = counters[i];
+            }
+        }
+
+        return words[maxIndex];
+    }
+
+
+    //4
+    public static boolean validate(String address) {
+
+        String[] protocols = {"http://", "https://"};
+        String[] domains = {".com", ".org", ".net"};
+        String[] symbols = {"$", "%", "&", "&"};
+
+        return validateProtocol(address, protocols) && validateDomains(address, domains) && validateSymbols(address, symbols);
+    }
+
+
+
+    public static int[] countDuplicates(String input, String[] words) {
+
+        String[] strings = input.split(" ");
+        int[] res = new int[words.length];
+
+        for (String string : strings) {
+            for (int i = 0; i < words.length; i++) {
+                if (string.equals(words[i]))
+                    res[i]++;
+            }
+        }
+
+        return res;
+    }
+
+    public static String deleteDuplicates(String input) {
+
+        String[] words = input.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j]))
+                    words[j] = "";
+            }
+        }
+
+        String res = "";
+
+        for (String word : words) {
+            res += word;
+
+            if (! word.isEmpty())
+                res += " ";
+        }
+
+        return res;
+    }
+
+    public static boolean validateProtocol(String address, String[] protocols) {
+
+        for (String protocol : protocols) {
+            if (address.startsWith(protocol))
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean validateDomains(String address, String[] domains) {
+
+        for (String domain : domains) {
+            if (address.endsWith(domain))
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean validateSymbols(String address, String[] symbols) {
+
+        for (String symbol : symbols) {
+            if (address.contains(symbol))
+                return false;
+        }
+
+        return true;
+    }
+
+}
