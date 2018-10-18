44ade67f087a19ecb0a9cfa74676fed58ba20fb8
dymychUA <dymych@outlook.com>
2018-10-18 13:09:44 +0300

    lesson 15 HW - 7

---
+++ src/lesson18/Definition.java
@@ -0,0 +1,12 @@
+package lesson18;
+
+public class Definition {
+
+    private void notifyUsers(String[] userEmails) {
+
+        for (String email : userEmails) {
+            //send email to user - error
+        }
+    }
+
+}
---
+++ src/lesson18/exercise1/Storage.java
@@ -0,0 +1,14 @@
+package lesson18.exercise1;
+
+public abstract class Storage {
+
+    private String[] files;
+
+    public String[] getFiles() {
+        return files;
+    }
+
+    public void setFiles(String[] files) {
+        this.files = files;
+    }
+}
