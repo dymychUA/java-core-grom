0af3019c7e3da88aaff62ca8c6e0655e96a16017
dymychUA <dymych@outlook.com>
2018-11-23 15:47:44 +0200

    lesson 15 HW - 7

--- src/lesson20/task2/Demo.java
+++ src/lesson20/task2/Demo.java
@@ -9,10 +9,10 @@ public class Demo {
 
         Controller controller = new Controller();
 
-        Transaction tr0 = new Transaction(100, "Kiev", 40, "50 hrn", TransactionType.INCOME, new Date());
+        Transaction tr0 = new Transaction(100, "Kiev", 40, "40 hrn", TransactionType.INCOME, new Date());
 
-        Transaction tr1 = new Transaction(101, "Odessa", 5, "Allo. New phone. 10 hrn", TransactionType.OUTCOME, new Date());
-        Transaction tr2 = new Transaction(102, "Kiev", 5, "Allo. New phone. 10 hrn", TransactionType.OUTCOME, new Date());
+        Transaction tr1 = new Transaction(101, "Odessa", 5, "Allo. New phone. 5 hrn", TransactionType.OUTCOME, new Date());
+        Transaction tr2 = new Transaction(102, "Kiev", 5, "Allo. New phone. 5 hrn", TransactionType.OUTCOME, new Date());
         Transaction tr3 = new Transaction(103, "Lviv", 20, "Allo. New phone. 20 hrn", TransactionType.OUTCOME, new Date());
 
         Transaction tr4 = new Transaction(104, "Kiev", 50, "Allo. New phone. 50 hrn", TransactionType.OUTCOME, new Date());
